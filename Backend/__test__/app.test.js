f51cd45cdb63aea839cae95007816cd0be0b73b0
Tahir Baku <eri.baku3@gmail.com>
2021-10-22 16:45:47 +0200

    Added the supertest in JEST

--- Backend/__test__/app.test.js
+++ Backend/__test__/app.test.js
@@ -1,17 +1,20 @@
 import axios from "axios";
 const mongoose = require("mongoose");
+const request = require("supertest");
 
-const id = "6171cc801b71f46426c0b667";
+const id = "6172cdd058e6bc1cc2fe28f5";
+const urlBase = "http://localhost:5000";
 
 describe("Item API testing", () => {
   it("Get all items and check if the array is completed", async () => {
-    let allItem = await axios
-      .get("http://localhost:5000/item")
+    let allItems = await request(urlBase)
+      .get("/item")
+      .expect(200)
       .then((res) => {
         return res.data;
       })
       .catch((err) => console.log(err));
-    expect(allItem).not.toBe([]);
+    expect(allItems).not.toBe([]);
   });
 
   it("Add an item and check if the database is updated", async () => {
@@ -44,45 +47,43 @@ describe("Item API testing", () => {
     expect(newItems).not.toBe(oldItems);
   });
 
-  // it("Update an item and get that item API ", async () => {
-  //   let item = await axios
-  //     .get("http://localhost:5000/item/getItem/" + id)
-  //     .then((res) => {
-  //       return res.data;
-  //     })
-  //     .catch((err) => console.log(err));
-  //   const itemUpdated = {
-  //     name: "TestUpdated",
-  //     unit: 101,
-  //     price: 701,
-  //     image: "RandomLink",
-  //     category: "61546a9f560152d1b96b4687",
-  //   };
-  //   axios.patch("http://localhost:5000/item/updateItem/" + id, {
-  //     name: itemUpdated.name,
-  //     unit: itemUpdated.unit,
-  //     price: itemUpdated.price,
-  //     image: itemUpdated.image,
-  //     category: itemUpdated.category,
-  //   });
-  //   let newUpdatedItem = await axios
-  //     .get("http://localhost:5000/item/getItem/" + id)
-  //     .then((res) => {
-  //       return res.data;
-  //     });
-  //   expect(item).not.toEqual(newUpdatedItem);
-  // });
+  it("Update an item and get that item API ", async () => {
+    let item = await axios
+      .get("http://localhost:5000/item/getItem/" + id)
+      .then((res) => {
+        return res.data;
+      })
+      .catch((err) => console.log(err));
+    const itemUpdated = {
+      name: "TestUpdated",
+      unit: 101,
+      price: 701,
+      image: "RandomLink",
+      category: "61546a9f560152d1b96b4687",
+    };
+    axios.patch("http://localhost:5000/item/updateItem/" + id, {
+      name: itemUpdated.name,
+      unit: itemUpdated.unit,
+      price: itemUpdated.price,
+      image: itemUpdated.image,
+      category: itemUpdated.category,
+    });
+    let newUpdatedItem = await axios
+      .get("http://localhost:5000/item/getItem/" + id)
+      .then((res) => {
+        return res.data;
+      });
+    expect(item).not.toEqual(newUpdatedItem);
+  });
 
   // it("Delete and item and check if that item is deleted and find element by that ID", async () => {
   //   let item = await axios
-  //     .get("http://localhost:5000/item/getItem/"+id)
+  //     .get("http://localhost:5000/item/getItem/" + id)
   //     .then((res) => {
   //       return res.data;
   //     })
   //     .catch((err) => console.log(err));
-  //   axios.delete(
-  //     "http://localhost:5000/item/deleteItem/"+id
-  //   );
+  //   axios.delete("http://localhost:5000/item/deleteItem/" + id);
   //   let newItems = await axios
   //     .get("http://localhost:5000/item")
   //     .then((res) => {
@@ -91,18 +92,18 @@ describe("Item API testing", () => {
   //     .catch((err) => console.log(err));
   //   expect(newItems).not.toContain(item);
   // });
-})
+});
 
 describe("Category automatic testing API-s", () => {
-  it("Get all categoryies",async ()=>{
-      let allCategories = await axios
-        .get("http://localhost:5000/category")
-        .then((res) => {
-          return res.data;
-        })
-        .catch((err) => console.log(err));
-      expect(allCategories).not.toBe([]);
-    });
+  it("Get all categoryies", async () => {
+    let allCategories = await axios
+      .get("http://localhost:5000/category")
+      .then((res) => {
+        return res.data;
+      })
+      .catch((err) => console.log(err));
+    expect(allCategories).not.toBe([]);
+  });
   it("Add an Category and check if the database is updated", async () => {
     let oldCategories = await axios
       .get("http://localhost:5000/category")
